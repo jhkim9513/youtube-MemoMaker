83bd6d4b47e92712d43845ee2c01aed4ef19282f
김종환 <dondon9513@gmail.com>
2021-07-28 21:52:54 +0900

    modify header UI

--- memoMaker/src/components/header/header.jsx
+++ memoMaker/src/components/header/header.jsx
@@ -1,25 +1,41 @@
 import React, { memo } from "react";
+import Button from "../button/button";
 import styles from "./header.module.css";
 
 const Header = memo(({ onLogout, goToYoutube, goToMemoMaker }) => (
   <header className={styles.header}>
-    {onLogout && (
-      <button className={styles.logout} onClick={onLogout}>
-        Logout
-      </button>
-    )}
-    {true && (
-      <button className={styles.searchButton} onClick={() => goToYoutube()}>
-        search
-      </button>
-    )}
-    {true && (
-      <button className={styles.memoButton} onClick={() => goToMemoMaker()}>
-        Memo
-      </button>
-    )}
     <h1 className={styles.title}>Youtube Memo Maker</h1>
+    <div className={styles.buttonBox}>
+      {goToYoutube && (
+        <div className={styles.youtubeButton}>
+          <Button name="youtube 찾아보기" onClick={() => goToYoutube()} />
+        </div>
+      )}
+      {goToMemoMaker && (
+        <div className={styles.memoMakerButton}>
+          <Button name="메인으로" onClick={() => goToMemoMaker()} />
+        </div>
+      )}
+      {onLogout && (
+        <div className={styles.logout}>
+          <Button name="로그 아웃" onClick={() => onLogout()} />
+        </div>
+      )}
+    </div>
   </header>
 ));
 
 export default Header;
+
+/*
+<button className={styles.logout} onClick={onLogout}>
+  Logout
+</button>
+
+<button className={styles.searchButton} onClick={() => goToYoutube()}>
+  search
+</button>
+<button className={styles.memoButton} onClick={() => goToMemoMaker()}>
+  Memo
+</button>
+*/
