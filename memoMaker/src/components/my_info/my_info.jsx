c3e58d9b57803502ca4d37043a4030bef3f5628a
김종환 <dondon9513@gmail.com>
2021-10-03 18:00:27 +0900

    remove console.log

--- memoMaker/src/components/my_info/my_info.jsx
+++ memoMaker/src/components/my_info/my_info.jsx
@@ -14,7 +14,6 @@ const MyInfo = memo(
     const svgRef = useRef();
     const [loading, setLoading] = useState(true);
 
-    console.log(loading);
     /* Lifecycle */
     useEffect(() => {
       auth.onAuthChange((user) => {
